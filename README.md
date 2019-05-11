9e91703b41495ba7d457fd3f5ee4b257c27a1121
zd-zero <zaerald.zd@gmail.com>
2019-05-12 03:50:26 +0800

    :sparkles: Implement argparse

--- README.md
+++ README.md
@@ -3,15 +3,15 @@ Command line utility to easily create branch names for Git from a ticket name.
 
 ### Usage
 ```text
-$ gbranching [-t <ticket-type>] [-n <ticket-number>] -b <branch name>
-$ gbranching -b branch name 
+$ gbranching [-t <ticket-type>] [-n <ticket-number>] title
+$ gbranching title 
 ```
 
 ### Demo
 ```bash
-$ gbranching story 'Some new feature to implement'
+$ gbranching -t story 'Some new feature to implement'
 story/GB-some-new-feature-to-implement
 
-$ gbranching story 1234 'Some new feature to implement'
+$ gbranching -t story -n 1234 'Some new feature to implement'
 story/GB-1234-some-new-feature-to-implement
 ```
--- gbranching/__init__.py
+++ gbranching/__init__.py
@@ -3,26 +3,68 @@
 gbranching
 Command line utility to easily create branch names for Git from a ticket name.
 
+Usage
+```text
+$ gbranching [-t <ticket-type>] [-n <ticket-number>] title
+$ gbranching
+$ gbranching title
+```
+
 @author Zaerald Denze Lungos
 @version 1.0
+__doc__
 """
+import argparse
 import re
-import sys
+import textwrap
+from argparse import RawTextHelpFormatter
+
 import pyperclip
 
-args = sys.argv
 
-branch_name = str(args[1]).lower()
-branch_invalid_patterns = r'''
-    [.]|            # cannot start with a dot `.`
-    [@{(..)\\\?]|   # must not contain any of this characters
-    (@{)
-    [\t\r\n\v\f]|   # exclude other white spaces except space ` `
-    \-{2,}|         # remove consecutive `--`
-    [/(.lock)]$     # must not end with `/` `.lock`
-'''
+def transform_title(title):
+    title = title.lower()
+    branch_invalid_patterns = r'''
+        [.]|            # cannot start with a dot `.`
+        [@{(..)\\\?'"]|   # must not contain any of this characters
+        (@{)
+        [\t\r\n\v\f]|   # exclude other white spaces except space ` `
+        \-{2,}|         # remove consecutive `--`
+        [/(.lock)]$     # must not end with `/` `.lock`
+    '''
 
-branch_name = re.sub(branch_invalid_patterns, '', branch_name, flags=re.VERBOSE).strip().replace(' ', '-')
+    return re.sub(branch_invalid_patterns, '', title, flags=re.VERBOSE).strip().replace(' ', '-')
+
+
+parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter, description="""
+    gbranching a command line utility to easily transform ticket name to create branch names for Git. 
+""", epilog=textwrap.dedent("""
+    Author: Zaerald Denze Lungos
+    Version: 1.0
+"""))
+
+parser.add_argument('-t', '--type', choices=['story', 'bug', 'bau'],
+                    help='the ticket type ')
+parser.add_argument('-p', '--project',
+                    help='project name initials')
+parser.add_argument('-n', '--number', type=int,
+                    help='ticket number')
+parser.add_argument('title', help='title of the ticket')
+args = parser.parse_args()
+
+ticket_type = 'story'
+if args.type:
+    ticket_type = args.type
+
+project = ''
+if args.project:
+    project = f'{args.project}-'
+
+ticket_number = ''
+if args.number:
+    ticket_number = f'{args.number}-'
+
+branch_name = f'{ticket_type}/{project}{ticket_number}{transform_title(args.title)}'
 pyperclip.copy(branch_name)
 
-print(branch_name)
+print(f"'{branch_name}' is copied to clipboard!")
